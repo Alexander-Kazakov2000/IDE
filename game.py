c6cb23512f63d2e8b9ee0e6ebb96b8683003d3a6
Alexander-Kazakov2000 <mister.cahbka@ya.ru>
2023-12-13 17:54:10 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,15 @@
+import numpy as np
+number = np.random.randint(1, 101)
+count = 0
+
+while True:
+    count+=1
+    user_number = int(input("Введите число:"))
+    if user_number > number:
+        print ('Ваше число больше загаданого.')
+    elif user_number < number:
+        print ('Ваше число меньше загаданого.')
+    else:
+        print (f'Вы угдали загаданное число {number}, за {count} попыток')
+        break
+    
